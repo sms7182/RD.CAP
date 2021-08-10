e72f8f91bd05c545285fc6602f85d86c4482eed3
unknown <sms7182@gmail.com>
2021-08-10 16:32:27 +0430

    first commit for cap

---
+++ RD.Core.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RD.Core.API.Controllers
+{
+    
+    public class SampleEvent
+    {
+        public Guid Id { get; set; }
+    }
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly ICapPublisher _capBus;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,ICapPublisher capPublisher)
+        {
+            _logger = logger;
+           _capBus = capPublisher;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost("sendWithCAP")]
+        public async Task<ActionResult> SendWithCAP(Guid id)
+        {
+            var sampleEvent = new SampleEvent();
+            sampleEvent.Id = id;
+            await _capBus.PublishAsync(nameof(SampleEvent), sampleEvent);
+            return Ok(sampleEvent);
+        }
+
+        //[CapSubscribe(nameof(SampleEvent))]
+        //public void CheckReceivedMessage(SampleEvent sampleEvent)
+        //{
+
+        //}
+    }
+}
---
+++ RD.Core.CAP/CAPConfiuration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RD.Core.CAP
+{
+    public static class CAPConfiuration
+    {
+        public static IServiceCollection AddCAP(this IServiceCollection services)
+        {
+            services.AddCap(x =>
+            {
+                x.UseMongoDB(mo => { mo.DatabaseName = "CAPDB"; mo.DatabaseConnection = "mongodb://localhost:27017"; });
+                x.UseRabbitMQ("localhost:5672");
+
+               
+            });
+            return services;
+        }
+    }
+}
